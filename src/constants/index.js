e4fe0e788e38484bcdc19933e79e779ebba3f186
rachelharu <rachel.haru9@gmail.com>
2022-11-16 16:32:25 -0800

    create constants folder and create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,53 @@
+export const navlinks = [
+    {
+        title: 'Product',
+        links: [
+            {
+                name: 'Overview'
+            },
+            {
+                name: 'Pricing'
+            },
+            {
+                name: 'Marketplace'
+            },
+            {
+                name: 'Features'
+            },
+            {
+                name: 'Integrations'
+            },
+        ],
+    },
+    {
+        title: 'Company',
+        links: [
+            {
+                name: 'About'
+            },
+            {
+                name: 'Team'
+            },
+            {
+                name: 'Blog'
+            },
+            {
+                name: 'Career'
+            },
+        ],
+    },
+    {
+        title: 'Connect',
+        links: [
+            {
+                name: 'Contact'
+            },
+            {
+                name: 'Newsletter'
+            },
+            {
+                name: 'LinkedIn'
+            },
+        ],
+    },
+]
