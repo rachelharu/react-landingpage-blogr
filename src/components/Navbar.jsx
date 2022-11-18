569cfaccfd1e82b8c0690c1f2488971061583507
rachelharu <rachel.haru9@gmail.com>
2022-11-18 15:44:01 -0800

    update navbar dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import {
 } from '../assets';
 import { navLinks } from '../constants';
 
+
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
@@ -16,19 +17,18 @@ const Navbar = () => {
       <img src={logo} alt='blogr' className='w-[75px] h-[30px]' />
 
       <ul className='list-none sm:flex hidden ml-10 justify-start items-center flex-1'>
+
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-overpass
-        font-normal
-        text-[12px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
-      text-white`}
-          >
+            font-normal
+            text-[12px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
+          text-white`}>
             <a
               className='float-left'
               onClick={() => setToggle((prev) => !prev)}
-              href={`#${nav.id}`}
-            >
+              href={`#${nav.id}`}>
               {nav.title}
               <img
                 className='ml-2 mt-1 cursor-pointer float-right w-[9px] h-[6px]'
@@ -37,22 +37,26 @@ const Navbar = () => {
             </a>
 
             <div className={`${toggle ? 'hidden' : 'relative'} mr-10`}>
-              <ul className='list-none mt-10'>
+              
+              <ul className='list-none mt-10 absolute'>
                 {nav.links.map((link, index) => (
                   <li
                     key={link.name}
-                    className={`font-overpass text-black ${
-                      index !== nav.links.length - 1 ? 'mb-4' : 'mb-0'
-                    }`}
-                  >
+                    className={`font-overpass text-black  ${
+                      index !== nav.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
                     {link.name}
                   </li>
                 ))}
               </ul>
+              
             </div>
-          </li>
+          </li>              
+          
+
         ))}
       </ul>
+
+      
     </nav>
   );
 };
