0c436f97b1c16410603d5a7ff4249e36a1dff859
rachelharu <rachel.haru9@gmail.com>
2022-11-18 14:45:30 -0800

    update navbar to show lists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,24 +8,53 @@ import {
 } from '../assets';
 import { navLinks } from '../constants';
 
-const Navbar = () => (
-  <nav className='w-full flex py-6 ml-10 justify-between items-center navbar'>
-    <img src={logo} alt='blogr' className='w-[75px] h-[30px]' />
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
 
-    <ul className='list-none sm:flex hidden ml-10 justify-start items-center flex-1'>
-      {navLinks.map((nav, index) => (
-        <li
-        key={nav.id}
-        className={`font-overpass
+  return (
+    <nav className='w-full flex py-6 ml-10 justify-between items-center navbar'>
+      <img src={logo} alt='blogr' className='w-[75px] h-[30px]' />
+
+      <ul className='list-none sm:flex hidden ml-10 justify-start items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-overpass
         font-normal
         text-[12px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
-      text-white`}>
-          <a className="float-left" href={`#${nav.id}`}>{nav.title}</a>
-          <img className='ml-5 mt-1 cursor-pointer float-right'src={iconArrowLight} />
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+      text-white`}
+          >
+            <a
+              className='float-left'
+              onClick={() => setToggle((prev) => !prev)}
+              href={`#${nav.id}`}
+            >
+              {nav.title}
+              <img
+                className='ml-2 mt-1 cursor-pointer float-right w-[9px] h-[6px]'
+                src={iconArrowLight}
+              />
+            </a>
+
+            <div className={`${toggle ? 'hidden' : 'relative'} mr-10`}>
+              <ul className='list-none mt-10'>
+                {nav.links.map((link, index) => (
+                  <li
+                    key={link.name}
+                    className={`font-overpass text-black ${
+                      index !== nav.links.length - 1 ? 'mb-4' : 'mb-0'
+                    }`}
+                  >
+                    {link.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
