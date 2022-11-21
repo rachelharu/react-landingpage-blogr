9bed5faf825fb74768dd1d1d8900de639c2f0e70
rachelharu <rachel.haru9@gmail.com>
2022-11-21 10:20:39 -0800

    finish dropdown menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,8 @@ import {
 } from '../assets';
 import { navLinks } from '../constants';
 
-
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
+  const [openId, setOpenId] = useState(undefined);
 
   return (
     <nav className='w-full flex py-6 ml-10 justify-between items-center navbar'>
@@ -23,32 +22,32 @@ const Navbar = () => {
             key={nav.id}
             className={`font-overpass
             font-normal
+            hover:underline
             text-[12px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
           text-white`}>
             <a
               className='float-left'
-              onClick={() => setToggle((prev) => !prev)}
+              onClick={() => setOpenId((prev) => (prev === nav.id ? undefined : nav.id))}
               href={`#${nav.id}`}>
               {nav.title}
               <img
                 className='ml-2 mt-1 cursor-pointer float-right w-[9px] h-[6px]'
                 src={iconArrowLight}
+                onClick= {() => setSelected(iconArrowDark)}
               />
             </a>
 
-            <div className={`${toggle ? 'hidden' : 'relative'} mr-10`}>
-              
-              <ul className='list-none mt-10 absolute'>
+            <div className={`${openId !== nav.id ? "hidden" : "relative"} mr-10`}>
+              <ul className='list-none mt-10 absolute dropdown-background'>
                 {nav.links.map((link, index) => (
                   <li
                     key={link.name}
-                    className={`font-overpass text-black  ${
+                    className={`font-ubuntu font-normal text-black cursor-pointer hover:font-bold ${
                       index !== nav.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
                     {link.name}
                   </li>
                 ))}
               </ul>
-              
             </div>
           </li>              
           
