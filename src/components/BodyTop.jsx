1b7f9b93dc35d6dc0f98750396d81fc9f7ef4498
rachelharu <rachel.haru9@gmail.com>
2022-11-28 14:25:52 -0800

    recommit last fix mistake in content alignment for bodytop

--- src/components/BodyTop.jsx
+++ src/components/BodyTop.jsx
@@ -10,7 +10,7 @@ const BodyTop = () => {
        mt-10 space-y-0 md:flex-row'>
       
         <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
-          <h3 className='mt-10 text-headingColor font-medium text-[24px] md:text-center'>Introducing an extensible editor</h3>
+          <h3 className='mt-20 text-headingColor font-medium text-[24px] text-center md:text-left'>Introducing an extensible editor</h3>
           <p className='text-bodyColor font-light items-center text-center sm:text-left'>
             Blogr features an exceedingly intuitive interface which lets you
             focus on one thing: creating content. The editor supports management
@@ -20,7 +20,7 @@ const BodyTop = () => {
             blog.
           </p>
 
-          <h3 className='mt-10 text-headingColor font-medium text-[24px] md:text-center'>Robust content management</h3>
+          <h3 className='mt-10 text-headingColor font-medium text-[24px] text-center md:text-left'>Robust content management</h3>
           <p className='text-bodyColor font-light items-center text-center sm:text-left'>
             Flexible content management enables users to easily move through
             posts. Increase the usability of your blog by adding customized
