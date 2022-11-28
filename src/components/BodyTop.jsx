bafc8d715138957fea4c650f8c9d447ea80e434e
rachelharu <rachel.haru9@gmail.com>
2022-11-28 15:19:36 -0800

    finish bodytop section

--- src/components/BodyTop.jsx
+++ src/components/BodyTop.jsx
@@ -3,15 +3,14 @@ import { illustrationEditorDesktop, illustrationEditorMobile } from '../assets';
 
 const BodyTop = () => {
   return (
-    <div>
-      <h2 className='text-center mt-24 text-[24px] text-headingColor font-medium'>Designed for the future</h2>
-      <div
-        className='container flex flex-col-reverse px-6 mx-auto 
-       mt-10 space-y-0 md:flex-row'>
+    <div className='w-full'>
+      <h2 className='text-center mt-36 text-[30px] text-headingColor font-medium'>Designed for the future</h2>
       
-        <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
-          <h3 className='mt-20 text-headingColor font-medium text-[24px] text-center md:text-left'>Introducing an extensible editor</h3>
-          <p className='text-bodyColor font-light items-center text-center sm:text-left'>
+      <div className='flex flex-col-reverse px-6 mx-auto 
+      space-y-0 md:flex-row'>
+        <div className='mt-32 mb-32 md:w-1/2'>
+          <h3 className='mt-20 text-headingColor font-medium text-[25px] text-center md:text-left'>Introducing an extensible editor</h3>
+          <p className='text-bodyColor font-light mt-2 items-center text-center sm:text-left'>
             Blogr features an exceedingly intuitive interface which lets you
             focus on one thing: creating content. The editor supports management
             of multiple blogs and allows easy manipulation of embeds such as
@@ -20,8 +19,8 @@ const BodyTop = () => {
             blog.
           </p>
 
-          <h3 className='mt-10 text-headingColor font-medium text-[24px] text-center md:text-left'>Robust content management</h3>
-          <p className='text-bodyColor font-light items-center text-center sm:text-left'>
+          <h3 className='mt-10 text-headingColor font-medium text-[25px] text-center md:text-left'>Robust content management</h3>
+          <p className='text-bodyColor font-light mt-2 items-center text-center sm:text-left'>
             Flexible content management enables users to easily move through
             posts. Increase the usability of your blog by adding customized
             categories, sections, format, or flow. With this functionality,
@@ -30,11 +29,11 @@ const BodyTop = () => {
         </div>
 
 
-        <div className=''>
-          <picture>
+        <div className='w-full h-full relative'>
+          <picture className='relative '>
             <source media='(min-width:650px)'
              srcset={illustrationEditorDesktop}></source>
-             <img src={illustrationEditorMobile} />
+             <img className='lg:ml-96' src={illustrationEditorMobile} />
           </picture>
         </div>
 
