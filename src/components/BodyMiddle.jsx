718aad889acc9c1cfff2104b6ca93ffdea7c8226
rachelharu <rachel.haru9@gmail.com>
2022-11-30 13:49:37 -0800

    fix position of items in bodymiddle

--- src/components/BodyMiddle.jsx
+++ src/components/BodyMiddle.jsx
@@ -3,8 +3,8 @@ import { bgPatternCircles, illustrationPhones } from '../assets';
 
 const BodyMiddle = () => {
   return (
-    <div className='relative flex h-[500px]'>
-      <div className='overflow-hidden h-[450px] rounded-borders'>
+    <div className='relative flex h-[500px] left-0 md:left-[40px]'>
+      <div className='h-[450px] rounded-borders'>
         <img
           className='relative h-[1000px] w-[950px] md:right-[130px] bottom-[570px] mobile-circle-image'
           src={bgPatternCircles}
@@ -21,7 +21,7 @@ const BodyMiddle = () => {
          justify-center items-center'>
       <h3 className='font-medium text-[34px] text-white text-center md:text-left mt-40 md:mt-0 mb-2 md:mb-0'>
       State of the Art Infrastructure</h3>
-      <p className='text-[14px] text-white text-center md:text-left'>
+      <p className='text-[14px] text-white text-center md:text-left px-10 md:px-0 md:pr-32'>
         With reliability and speed in mind, worldwide data centers provide the
         backbone for ultra-fast connectivity. This ensures your site will load
         instantly, no matter where your readers are, keeping your site
